0f8d86911e355c01fcb125d7d90755a9b386b734
hallovon <lzb_00@126.com>
2023-03-31 09:38:59 +0800

    strict equal

--- 10-Fundamentals-Part-1/script.js
+++ 10-Fundamentals-Part-1/script.js
@@ -14,23 +14,36 @@
 // console.log("23" + "10" + 3); // "23103"
 // console.log("23" / "2"); // 11.5
 
-// 5 falsy values: 0, "", undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(""));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));
+// // 5 falsy values: 0, "", undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
 
-const money = 0;
-if (money) {
-  console.log("Dont spend if all");
-} else {
-  console.log("You should get a job");
+// const money = 0;
+// if (money) {
+//   console.log("Dont spend if all");
+// } else {
+//   console.log("You should get a job");
+// }
+
+// let height;
+// if (height) {
+//   console.log("Yay, height is defined");
+// } else {
+//   console.log("height is UNDEFINED");
+// }
+
+const age = 18;
+if (age === "18") {
+  console.log("You just become an adult");
 }
 
-let height;
-if (height) {
-  console.log("Yay, height is defined");
-} else {
-  console.log("height is UNDEFINED");
+if (age == "18") {
+  console.log("You just become an adult");
 }
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
