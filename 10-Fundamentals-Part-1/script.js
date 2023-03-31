bdd6413fb24a7672f1586830eeb9fe8edfadcded
hallovon <lzb_00@126.com>
2023-03-31 09:23:21 +0800

    类型转换

---
+++ 10-Fundamentals-Part-1/script.js
@@ -0,0 +1,15 @@
+// tyoe conversion
+const inputYear = "1991";
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18);
+
+console.log(Number("lee"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type corecion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3); // 10
+console.log("23" + "10" + 3); // "23103"
+console.log("23" / "2"); // 11.5
