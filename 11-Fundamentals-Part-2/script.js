7389db0f749edc4a1fad8fe93854f44676e14b39
hallovon <lzb_00@126.com>
2023-04-03 09:50:47 +0800

    js array

--- 11-Fundamentals-Part-2/script.js
+++ 11-Fundamentals-Part-2/script.js
@@ -7,28 +7,35 @@
 
 // logger();
 
-function logger() {
-  console.log("hello world");
-}
+// function logger() {
+//   console.log("hello world");
+// }
 
-// function declaration
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// // function declaration
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-// function expression
-const fruitProcessor2 = function (apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-};
+// // function expression
+// const fruitProcessor2 = function (apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// };
 
-// arrow function
-const fruitProcessor3 = (apples, oranges) => {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-};
+// // arrow function
+// const fruitProcessor3 = (apples, oranges) => {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// };
 
-console.log(fruitProcessor(5, 0));
-console.log(fruitProcessor2(3, 2));
-console.log(fruitProcessor3(4, 1));
+// console.log(fruitProcessor(5, 0));
+// console.log(fruitProcessor2(3, 2));
+// console.log(fruitProcessor3(4, 1));
+
+const friends = ["micheal", "steven", "peter"];
+console.log(friends);
+
+const years = new Array(1991, 1884, 2008, 20200);
+console.log(years);
+  
